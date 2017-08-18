bfc8b3430b0c02a5356568d3b04d89ad7e0ce4b2
Sean <sean@irorche.local>
2017-08-18 17:55:06 -0400

    add block breaker

---
+++ Block Breaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("COLLIDED!!!");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("TRIGGERED...");
+        var level = new LevelManager();
+        level.LoadLevel("Lose");
+    }
+}
---
+++ Block Breaker/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+    public static MusicPlayer instance;
+
+	// Use this for initialization
+	void Awake () {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
